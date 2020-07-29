de16eafa1aad18483219b44370636992b5e082b5
niushuang <shuang.niu@moefantasy.co.jp>
2020-07-29 18:28:12 +0900

    add immortal shield to enemy3

---
+++ Assets/Scripts/Buff/ImmortalShield.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImmortalShield : BuffBase
+{
+    public int percentage { get; private set; }
+
+    public ImmortalShield(int percentage)
+    {
+        this.percentage = percentage;
+    }
+    protected override void OnBeforeDamage(DamageInfo info)
+    {
+        var maxDamage = target.maxHp.Value * percentage / GameDefine.PERCENTAGE_MAX;
+        if (info.damage > maxDamage)
+        {
+            GameLogger.AddLog($"{target.name} immortal shield active, cut damage from {info.damage} to {maxDamage}");
+            info.damage = maxDamage;
+        }
+    }
+}
--- Assets/Scripts/Character/EnemyType1.cs
+++ Assets/Scripts/Character/EnemyType1.cs
@@ -6,5 +6,9 @@ public class EnemyType1 : CharacterLogic
 {
     public EnemyType1(int characterId, Vector2Int pos, string name, int maxHp, Team team, int atk, int def, GameDefine.CharacterType characterType, int dodgeRate) : base(characterId, pos, name, maxHp, team, atk, def, characterType, dodgeRate)
     {
+        ImmortalShield buff = new ImmortalShield(200);
+        buff.Init(this, this, GameDefine.BuffTickType.Turn, true, -1);
+        AddBuff(buff);
     }
+
 }
